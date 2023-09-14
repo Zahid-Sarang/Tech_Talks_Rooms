f65f7f5e56ee1931e4e4bb1ed5af283c9149a8f3
zahid-soft <zahidsarang95@gmail.com>
2023-09-14 20:13:22 +0530

    interceptor created

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -16,6 +16,35 @@ const api = axios.create({
 |
 */
 
-export const sendOtp = (data) => api.post('/api/send-otp', data);
-export const verifyOtp = (data) => api.post('/api/verify-otp', data);
-export const activate = (data) => api.post('/api/activate', data);
+export const sendOtp = (data) => api.post("/api/send-otp", data);
+export const verifyOtp = (data) => api.post("/api/verify-otp", data);
+export const activate = (data) => api.post("/api/activate", data);
+
+/* INTERCEPTORS */
+api.interceptors.response.use(
+	(config) => {
+		return config;
+	},
+	async (error) => {
+		const originalRequest = error.config;
+		if (
+			error.response.status === 401 &&
+			originalRequest &&
+			!originalRequest._isRetry
+		) {
+			originalRequest.isRetry = true;
+			try {
+				await axios.get(`${process.env.REACT_APP_API_URL}/api/refresh-Token`, {
+					withCredentials: true,
+				});
+				return api.request(originalRequest);
+			} catch (error) {
+				console.log("interceptors error");
+				console.log(error);
+			}
+		}
+		throw error;
+	}
+);
+
+export default api
